bdb71bd810004ac1426e32b77939c612da28a970
Jesse <jdmartin28200@gmail.com>
2019-01-12 23:31:34 -0600

    Added edit_character and changed create_character 1/12/19||23:30

--- Experiment_Area.py
+++ Experiment_Area.py
@@ -65,15 +65,12 @@ def create_character():
     pk_experience_ttl = input('Enter total experience: ')
 
     new_character = Character(ch_owner=pk_owner, ch_name=pk_name, ch_class=valid_class_pk, ch_level=pk_level,
-                    ch_race=pk_race, ch_alignment=pk_alignment, ch_experience_ttl=pk_experience_ttl)
-
+                              ch_race=pk_race, ch_alignment=pk_alignment, ch_experience_ttl=pk_experience_ttl)
     return new_character
 
 
 def edit_character():
     creation = False
-
-#    new_character = Character()
 
     edit_menu = ['1. Name', '2. Class', '3. Level',
                      '4. Player', '5. Race', '6. Alignment',
@@ -81,40 +78,39 @@ def edit_character():
                      ]
 
     while not creation:
-        for item in creation_menu:
+        for item in edit_menu:
             print(item)
         set_info = int(input('Select menu item number: '))
 
         if set_info == 1:
-            pk_name = input('Enter character name: ')
-            pk_name = 'Name: ' + pk_name
-#            new_character.ch_name = pk_name
+            pk_name = input('Enter new character name: ')
+#            edit_character.ch_name = pk_name
         elif set_info == 2:
             valid_cl = True
             while valid_cl:
-                pk_class = input('Enter character class: ')
+                pk_class = input('Enter new character class: ')
                 valid_cl = class_list(pk_class)
-#                new_character.ch_class = pk_class
+#                edit_character.ch_class = pk_class
         elif set_info == 3:
-            pk_level = input('Enter character level: ')
+            pk_level = input('Enter new character level: ')
             pk_level = 'Level: ' + pk_level
-#            new_character.ch_level = pk_level
+#            edit_character.ch_level = pk_level
         elif set_info == 4:
-            pk_owner = input('Enter player\'s name: ')
+            pk_owner = input('Enter new player\'s name: ')
             pk_owner = 'player: ' + pk_owner
-#            new_character.ch_owner = pk_owner
+#            edit_character.ch_owner = pk_owner
         elif set_info == 5:
-            pk_race = input('Enter character race: ')
+            pk_race = input('Enter new character race: ')
             pk_race = 'Race: ' + pk_race
-#            new_character.ch_race = pk_race
+#            edit_character.ch_race = pk_race
         elif set_info == 6:
-            pk_alignment = input('Enter character\'s alignment: ')
+            pk_alignment = input('Enter new character\'s alignment: ')
             pk_alignment = 'Alignment: ' + pk_alignment
-#            new_character.ch_alignment = pk_alignment
+#            edit_character.ch_alignment = pk_alignment
         elif set_info == 7:
-            pk_experience_ttl = input('Enter total experience: ')
+            pk_experience_ttl = input('Enter new experience total: ')
             pk_experience_ttl = 'Experience: ' + pk_experience_ttl
-#            new_character.ch_experience_ttl = pk_experience_ttl
+#            edit_character.ch_experience_ttl = pk_experience_ttl
 
 # broken, please fix me please
 '''
