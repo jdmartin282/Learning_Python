b559f69dc271144e20955b8860fd150bca91a88b
Travis Gerken <gerken.travis@gmail.com>
2019-01-14 21:13:20 -0900

    Simplified Most Classes and naming conventions.
    Removed a lot of the bulky code
    
    Used Pickle instead of Json to retain objects instead of just dictionaries.
    
    Added in Character Editing to Main Function with Recursion based on `Character`.__slots__

--- Character.py
+++ Character.py
@@ -1,13 +1,32 @@
 class Character:
-    def __init__(self, ch_owner, ch_name, ch_class, ch_level,
-                 ch_race, ch_alignment, ch_experience_ttl):
-        self.ch_owner = ch_owner
-        self.ch_name = ch_name
-        self.ch_class = ch_class
-        self.ch_level = ch_level
-        self.ch_race = ch_race
-        self.ch_alignment = ch_alignment
-        self.ch_experience_ttl = ch_experience_ttl
+
+    __slots__ = ['player', 'name', 'job', 'level', 'race', 'alignment', 'experience']
+
+    def __init__(self, player, name, job, race, alignment, level=1, experience=0):
+        self.player = player
+        self.name = name
+        self.job = job
+        self.level = level
+        self.race = race
+        self.alignment = alignment
+        self.experience = experience
 
 
+class Warlock(Character):
+    def __init__(self, patron, player, name, job, race, alignment, level=1, experience=0):
+        super().__init__(player, name, job, race, alignment, level=level, experience=experience)
+        self.patron = patron
 
+
+if __name__ == '__main__':
+    x = Character('Hatchi', 'Gremlock', 'Warlock', 'Tiefling', 'Chaotic Neutral')
+    choice = input(f'Select Attribute to Edit: {x.__slots__}\n')
+    if choice in x.__slots__:
+        print(f'{choice}: {x.__getattribute__(choice)}')
+        value = input(f'What would you like the new value for {choice} to be? ')
+        print(f'{choice}: {value}')
+        confirm = input('Are you sure you would like to save these changes? (Y/N): ')
+        if confirm.upper() == 'Y':
+            x.__setattr__(choice, value)
+            print('Changes Saved')
+            print(f'{choice}: {x.__getattribute__(choice)}')
