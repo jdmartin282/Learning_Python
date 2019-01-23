c566e830f46c0f51038f8e14cc0ff977ef7bec21
Travis Gerken <gerken.travis@gmail.com>
2019-01-22 20:14:05 -0900

    Added class Stats as well as a stat argument for Character

--- Character.py
+++ Character.py
@@ -1,32 +1,36 @@
+class Stats:
+
+    __slots__ = ['constitution', 'charisma', 'dexterity', 'intelligence', 'strength', 'wisdom']
+
+    def __init__(self, constitution, charisma, dexterity, intelligence, strength, wisdom):
+        self.constitution = constitution
+        self.charisma = charisma
+        self.dexterity = dexterity
+        self.intelligence = intelligence
+        self.strength = strength
+        self.wisdom = wisdom
+
+
 class Character:
 
-    __slots__ = ['player', 'name', 'job', 'level', 'race', 'alignment', 'experience']
+    __slots__ = ['player', 'name', 'job', 'level', 'race', 'stats', 'alignment', 'experience']
 
-    def __init__(self, player, name, job, race, alignment, level=1, experience=0):
+    def __init__(self, player, name, job, race, alignment, stats: Stats, level=1, experience=0):
         self.player = player
         self.name = name
         self.job = job
         self.level = level
         self.race = race
+        self.stats = stats
         self.alignment = alignment
         self.experience = experience
 
 
 class Warlock(Character):
-    def __init__(self, patron, player, name, job, race, alignment, level=1, experience=0):
-        super().__init__(player, name, job, race, alignment, level=level, experience=experience)
+    def __init__(self, patron, player, name, job, race, alignment, stats: Stats, level=1, experience=0):
+        super().__init__(player, name, job, race, alignment, stats=stats, level=level, experience=experience)
         self.patron = patron
 
 
 if __name__ == '__main__':
-    x = Character('Hatchi', 'Gremlock', 'Warlock', 'Tiefling', 'Chaotic Neutral')
-    choice = input(f'Select Attribute to Edit: {x.__slots__}\n')
-    if choice in x.__slots__:
-        print(f'{choice}: {x.__getattribute__(choice)}')
-        value = input(f'What would you like the new value for {choice} to be? ')
-        print(f'{choice}: {value}')
-        confirm = input('Are you sure you would like to save these changes? (Y/N): ')
-        if confirm.upper() == 'Y':
-            x.__setattr__(choice, value)
-            print('Changes Saved')
-            print(f'{choice}: {x.__getattribute__(choice)}')
+    pass
