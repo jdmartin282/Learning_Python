6cc4425b51e01202e7a6ff088d967dad7a623e84
Jesse <jdmartin28200@gmail.com>
2019-02-03 20:56:30 -0600

    Updated Create/Edit/Main functions 2/3/19||20:56

--- Character.py
+++ Character.py
@@ -1,11 +1,29 @@
 class Character:
-    def __init__(self, ch_owner, ch_name, ch_class, ch_level,
-                 ch_race, ch_alignment, ch_experience_ttl):
-        self.ch_owner = ch_owner
-        self.ch_name = ch_name
-        self.ch_class = ch_class
-        self.ch_level = ch_level
-        self.ch_race = ch_race
-        self.ch_alignment = ch_alignment
-        self.ch_experience_ttl = ch_experience_ttl
+
+    __slots__ = ['self', 'player', 'name', 'job', 'race', 'alignment', 'stats', 'level', 'experience']
+
+    def __init__(self, player, name, job, race, alignment, level=1, experience=0):
+        self.player = player
+        self.name = name
+        self.job = job
+        self.level = level
+        self.race = race
+        self.alignment = alignment
+        self.experience = experience
+        self.stats = Stats()
+
+
+class Stats:
+
+    __slots__ = ['strength', 'dexterity', 'wisdom', 'intelligence', 'charisma', 'constitution']
+
+    def __init__(self, strength, dexterity, wisdom, intelligence, charisma, constitution):
+        self.strength = strength
+        self.dexterity = dexterity
+        self.wisdom = wisdom
+        self.intelligence = intelligence
+        self.charisma = charisma
+        self.constitution = constitution
+
+
 
